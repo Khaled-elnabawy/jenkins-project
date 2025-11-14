9c0037afc0e5da98d7e4a9129033ffc2d2d07d14
Khaled-elnabawy <167560059+Khaled-elnabawy@users.noreply.github.com>
2025-11-14 19:15:42 +0200

    Change S3 backend region from eu-east-1 to us-east-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "jenkins-s3-4429"
     key          = "eks/terraform.tfstate"
-    region       = "eu-east-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
