eba31721151b815567c5498e99a2c96417e82bc5
Khaled-elnabawy <167560059+Khaled-elnabawy@users.noreply.github.com>
2025-11-14 17:17:42 +0200

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "hello-devops-production-terraform-state"
+    bucket       = "jenkins-s3-4429"
     key          = "eks/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
